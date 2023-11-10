f55c95a670528cb8668f5e294f21552293f08d03
Ratchanon Panmas <qq.panmas@gmail.com>
2023-11-10 17:33:14 +0700

    Add vendor chunk splitting and manual chunking to vite.config.ts.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,42 @@
-import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    splitVendorChunkPlugin()
+  ],
   server: {
     watch: {
       usePolling: true
+    }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id: string) {
+          // React
+          if (id.includes('react')) {
+            return 'react';
+          }
+
+          // React Router
+          if (id.includes('react-router')) {
+            return 'react-router';
+          }
+
+          // Firebase
+          if (id.includes('firebase')) {
+            return 'firebase';
+          }
+
+          // Chakra UI
+          if (id.includes('@chakra-ui')) {
+            return 'chakra-ui';
+          }
+        }
+      }
     }
   }
 })
