8a899f14bcf57d3d992e1758dcea173394772bdd
Ratchanon Panmas <qq.panmas@gmail.com>
2023-11-10 20:54:03 +0700

    Add login page UI

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,6 +1,35 @@
+import logo from '@/assets/logo.svg';
+import { Button, Container, Flex, Heading, Image, Input, Link, Text } from "@chakra-ui/react";
+import { Link as RouterLink } from 'react-router-dom';
+
 function LoginPage() {
     return (
-        <div>LoginPage</div>
+        <Container maxW="sm" display="flex" h="100svh" w="100svw" alignItems="center" justifyContent="center" flexDirection="column" py={5}>
+            <Flex h={10} justify="center">
+                <Image src={logo} alt="WheelBus Logo" />
+            </Flex>
+            <Flex flex={1} direction="column" align="center" justify="center" w="full" rowGap={5}>
+                <Flex direction="column" align="center">
+                    <Heading as="h1" size="xl" fontWeight="bold" color="brand.500"> เข้าสู่ระบบ </Heading>
+                    <Text fontFamily="prompt" color="gray.500">กรอกเบอร์โทรศัพท์ที่ใช้งาน</Text>
+                </Flex>
+
+                <Flex direction="column" gap={5} w="full">
+                    <Input type="tel" maxLength={10} w="full" placeholder='เบอร์โทร' bgColor="brand.100" border="none" px={6} py={7} fontFamily="prompt" _focusVisible={{}} />
+                    <Button bgColor="brand.500" color="white" w="full" size="lg" shadow="md">
+                        เข้าสู่ระบบ
+                    </Button>
+                </Flex>
+            </Flex>
+            <Flex mt={20}>
+                <Text color="gray.500">
+                    ยังไม่มีบัญชีใช่ไหม?&nbsp;
+                    <Link as={RouterLink} to="/auth/register" color="brand.500" textDecor="underline">
+                        สมัครใช้งาน
+                    </Link>
+                </Text>
+            </Flex>
+        </Container >
     )
 }
 
