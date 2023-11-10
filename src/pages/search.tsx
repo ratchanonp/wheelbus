3c8b4fe637b30ba176bbf083f5722c5ad44f890f
Ratchanon Panmas <qq.panmas@gmail.com>
2023-11-11 01:25:30 +0700

    Add geolocation options and error handling to search page

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,4 +1,4 @@
-import { Flex, Spinner } from "@chakra-ui/react";
+import { Flex, Spinner, Text } from "@chakra-ui/react";
 import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
 import { useEffect, useState } from "react";
 
@@ -7,27 +7,35 @@ function SerchPage() {
     const [loading, setLoading] = useState(true);
     const [currentLocation, setCurrentLocation] = useState({ lat: 0, lng: 0 });
 
-    // Get Latitude and Longitude Geolocation
-    const getCurrentLocation = () => {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition((position) => {
-                const { latitude, longitude } = position.coords;
-                setCurrentLocation({ lat: latitude, lng: longitude })
-                setLoading(false);
-            });
-        }
 
-        return null
+
+    const options: PositionOptions = {
+        enableHighAccuracy: true,
+        timeout: 10000,
+        maximumAge: 0
+    }
+
+    const success = (position: GeolocationPosition) => {
+        const { latitude, longitude } = position.coords;
+        setCurrentLocation({ lat: latitude, lng: longitude })
+        setLoading(false);
+    }
+
+    const error = () => {
+        alert('Unable to retrieve your location');
     }
 
     useEffect(() => {
-        getCurrentLocation();
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(success, error, options);
+        }
     }, [])
 
     if (loading) {
         return (
             <Flex w="100svw" h="100svh" justify="center" align="center">
                 <Spinner size="xl" color="brand.500" />
+                <Text fontFamily="prompt" color="gray.500" ml={3}>กำลังค้นหาตำแหน่งปัจจุบันของคุณ</Text>
             </Flex>
         )
     }
