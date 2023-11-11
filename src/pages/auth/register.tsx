778afbd9ae1efa4e1b5b899badfad835e5666927
Ratchanon Panmas <qq.panmas@gmail.com>
2023-11-11 23:29:11 +0700

    Update RegisterPage component with confirm password field

--- src/pages/auth/register.tsx
+++ src/pages/auth/register.tsx
@@ -1,6 +1,6 @@
 import logo from '@/assets/logo.svg';
 import { auth } from '@/firebase/firebase';
-import { Button, Container, Flex, FormControl, Heading, Image, Input, Link, Text, useToast } from "@chakra-ui/react";
+import { Button, Container, Flex, FormControl, Heading, Image, Input, Link, Stack, Text, useToast } from "@chakra-ui/react";
 import { AuthError, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
 import { useRef } from 'react';
 import { Link as RouterLink, useNavigate } from 'react-router-dom';
@@ -15,6 +15,7 @@ function RegisterPage() {
     const lastname = useRef<HTMLInputElement>(null);
     const email = useRef<HTMLInputElement>(null);
     const password = useRef<HTMLInputElement>(null);
+    const confirmPassword = useRef<HTMLInputElement>(null);
 
     const handleRegister = async (e: React.FormEvent<HTMLDivElement>) => {
         e.preventDefault();
@@ -24,6 +25,18 @@ function RegisterPage() {
             lastname: lastname.current?.value ?? '',
             email: email.current?.value ?? '',
             password: password.current?.value ?? '',
+        }
+
+        if (data.password !== confirmPassword.current?.value) {
+            toast({
+                title: "เกิดข้อผิดพลาด",
+                description: "รหัสผ่านไม่ตรงกัน",
+                status: "error",
+                duration: 9000,
+                isClosable: true,
+                position: "top"
+            })
+            return
         }
 
         try {
@@ -61,29 +74,36 @@ function RegisterPage() {
                     <Text fontFamily="prompt" color="gray.500">กรอกข้อมูลด้านล่างเพื่อสมัครใช้งาน</Text>
                 </Flex>
 
-                <FormControl as="form" onSubmit={handleRegister} display="flex" flexDirection="column" gap={5} w="full">
-                    <Input
-                        id="firstname" name="firstname" type="text" placeholder='ชื่อ' autoComplete='given-name' ref={firstname}
-                        w="full" bgColor="brand.100" border="none" px={6} py={7} fontFamily="prompt" _focusVisible={{}}
-                    />
-                    <Input
-                        id="lastname" name="lastname" type="text" placeholder='นามสกุล' autoComplete='family-name' ref={lastname}
-                        w="full" bgColor="brand.100" border="none" px={6} py={7} fontFamily="prompt" _focusVisible={{}}
-                    />
-                    <Input
-                        id="email" name="email" type="email" placeholder='อีเมล' autoComplete='email' ref={email}
-                        w="full" bgColor="brand.100" border="none" px={6} py={7} fontFamily="prompt" _focusVisible={{}}
-                    />
-                    <Input
-                        id="password" name="password" type="password" placeholder='รหัสผ่าน' autoComplete='new-password' ref={password}
-                        w="full" bgColor="brand.100" border="none" px={6} py={7} fontFamily="prompt" _focusVisible={{}}
-                    />
-                    <Button type="submit" bgColor="brand.500" color="white" w="full" size="lg" shadow="md">
-                        ลงทะเบียนใช้งาน
-                    </Button>
+                <FormControl as="form" onSubmit={handleRegister}>
+                    <Stack spacing={2}>
+
+                        <Input
+                            id="firstname" name="firstname" type="text" placeholder='ชื่อ' autoComplete='given-name' ref={firstname}
+                            w="full" bgColor="brand.100" border="none" px={6} py={7} fontFamily="prompt" _focusVisible={{}}
+                        />
+                        <Input
+                            id="lastname" name="lastname" type="text" placeholder='นามสกุล' autoComplete='family-name' ref={lastname}
+                            w="full" bgColor="brand.100" border="none" px={6} py={7} fontFamily="prompt" _focusVisible={{}}
+                        />
+                        <Input
+                            id="email" name="email" type="email" placeholder='อีเมล' autoComplete='email' ref={email}
+                            w="full" bgColor="brand.100" border="none" px={6} py={7} fontFamily="prompt" _focusVisible={{}}
+                        />
+                        <Input
+                            id="password" name="password" type="password" placeholder='รหัสผ่าน' autoComplete='new-password' ref={password}
+                            w="full" bgColor="brand.100" border="none" px={6} py={7} fontFamily="prompt" _focusVisible={{}}
+                        />
+                        <Input
+                            id="confirm-password" name="confirm-password" type="password" placeholder='ยืนยันรหัสผ่าน' autoComplete='confirm-password' ref={confirmPassword}
+                            w="full" bgColor="brand.100" border="none" px={6} py={7} fontFamily="prompt" _focusVisible={{}}
+                        />
+                        <Button type="submit" bgColor="brand.500" color="white" w="full" size="lg" shadow="md">
+                            ลงทะเบียนใช้งาน
+                        </Button>
+                    </Stack>
                 </FormControl>
             </Flex>
-            <Flex mt={20}>
+            <Flex>
                 <Text color="gray.500">
                     มีบัญชีอยู่แล้วใช่ไหม?&nbsp;
                     <Link as={RouterLink} to="/auth/login" color="brand.500" textDecor="underline">
