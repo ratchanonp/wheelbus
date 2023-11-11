950a5fae8a52fe66eda6e56dd8a808abf840b5d7
Ratchanon Panmas <qq.panmas@gmail.com>
2023-11-11 19:16:45 +0700

    Add Stop interface and getStops utility function

---
+++ src/interfaces/stop.interface.ts
@@ -0,0 +1,5 @@
+export interface Stop {
+    lat: number;
+    lng: number;
+    name: string;
+}
---
+++ src/utils/getStops.ts
@@ -0,0 +1,72 @@
+import { db } from "@/firebase/firebase";
+import { Stop } from "@/interfaces/stop.interface";
+import { collection, getDocs } from "firebase/firestore";
+
+const stopCollection = collection(db, "stops");
+
+/**
+ * Get all stops from firestore
+ * @returns array of stops
+ */
+async function getStops(): Promise<Stop[]> {
+
+
+    const querySnapshot = await getDocs(stopCollection);
+
+    const stops: Stop[] = [];
+
+    querySnapshot.forEach((doc) => {
+        stops.push(doc.data() as Stop);
+    });
+
+    return stops;
+
+}
+
+/**
+ * Get stop by id
+ * @param id id of stop
+ * @returns stop
+ */
+async function getStopById(id: string): Promise<Stop> {
+
+    const querySnapshot = await getDocs(stopCollection);
+
+    let stop: Stop = {
+        lat: 0,
+        lng: 0,
+        name: ""
+    };
+
+    querySnapshot.forEach((doc) => {
+        if (doc.id === id) {
+            stop = doc.data() as Stop;
+        }
+    });
+
+    return stop;
+}
+
+
+/**
+ * Function to search for stops by name
+ * @param keyword keyword to search for stops name
+ * @returns array of stops that match the keyword
+ */
+
+async function serchStops(keyword: string): Promise<Stop[]> {
+
+    const stops: Stop[] = await getStops();
+
+    // Filter stops by name
+    const filteredStops = stops.filter((stop) => {
+        return stop.name.toLowerCase().includes(keyword.toLowerCase());
+    });
+
+    return filteredStops;
+}
+
+export {
+    getStopById, getStops, serchStops
+};
+
