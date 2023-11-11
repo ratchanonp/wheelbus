5fbe140e78b27fe191783ddb48df94cb10bb6ae1
Ratchanon Panmas <qq.panmas@gmail.com>
2023-11-11 11:48:53 +0700

    Add logo and UI elements to search page

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,4 +1,6 @@
-import { Flex, Spinner, Text } from "@chakra-ui/react";
+import logo from '@/assets/logo.svg';
+import { HamburgerIcon } from '@chakra-ui/icons';
+import { Box, Button, Container, Flex, HStack, Heading, Image, Spinner, Stack, Text } from "@chakra-ui/react";
 import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
 import { useEffect, useState } from "react";
 
@@ -29,21 +31,60 @@ function SerchPage() {
 
     if (loading) {
         return (
-            <Flex w="100svw" h="100svh" justify="center" align="center" direction="column" rowGap={10}>
+            <Container maxW="container.sm" display="flex" h="100svh" w="100svw" alignItems="center" justifyContent="center" flexDirection="column" py={5} gap={5}>
                 <Spinner size="xl" color="brand.500" />
                 <Text fontFamily="prompt" color="gray.500" ml={3}>กำลังค้นหาตำแหน่งปัจจุบันของคุณ</Text>
-            </Flex>
+            </Container>
         )
     }
 
     return (
-        <Flex w="100svw" h="100svh">
-            <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
-                <Map center={currentLocation} zoom={16} currentLocation={currentLocation}>
-                    <Marker position={currentLocation} />
-                </Map>
-            </APIProvider>
-        </Flex>
+        <Container maxW="container.sm" display="flex" h="100svh" w="100svw" p={0} position="relative" overflow="hidden">
+            <Flex w="full" h="100svh" zIndex={0}>
+                <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
+                    <Map
+                        center={currentLocation}
+                        zoom={16}
+                        disableDefaultUI={true}
+                    >
+                        <Marker position={currentLocation} />
+                    </Map>
+                </APIProvider>
+            </Flex>
+            <Flex h={20} justify="center" alignItems="center" w="100svw" p={5} position="absolute">
+                <HamburgerIcon w={8} h={8} color="brand.500" />
+                <Image h={12} flex={1} src={logo} alt="WheelBus Logo" />
+                <Box w={8} h={8} />
+            </Flex>
+
+            <Flex w="100svw" position="absolute" bottom={0} px={5}>
+                <Stack bgColor="brand.500" w="full" borderTopRadius="2xl" p={6} spacing={5}>
+                    <Flex bgColor="white" color="brand.500" w="full" p={5} borderRadius="lg" shadow="md" alignItems="center"
+                    >
+                        <Box w={3} h={3} borderRadius="sm" bgColor="red" />
+                        <Heading flex={1} textAlign="center" color="brand.500" size="md" fontWeight="medium">ไปไหนดี</Heading>
+                        <Box w={3} h={3} borderRadius="sm" bgColor="transparent" />
+                    </Flex>
+
+                    <Stack>
+                        <Heading color="white" size="sm" fontWeight="medium">สถานที่โปรด</Heading>
+
+                        <HStack>
+
+                            <Button bgColor="white" borderRadius="lg" p={3} w="full">
+                                บ้าน
+                            </Button>
+                            <Button bgColor="white" borderRadius="lg" p={3} w="full">
+                                ที่ทำงาน
+                            </Button>
+                            <Button bgColor="white" borderRadius="lg" p={3} w="full">
+                                โรงพยาบาล
+                            </Button>
+                        </HStack>
+                    </Stack>
+                </Stack>
+            </Flex>
+        </Container >
     )
 }
 
