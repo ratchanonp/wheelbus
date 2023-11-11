912c14bf9b1e893d1730aa37d70b1dc1e02f3923
Ratchanon Panmas <qq.panmas@gmail.com>
2023-11-11 11:48:32 +0700

    Add Chakra UI Icons and re-resizable library

--- vite.config.ts
+++ vite.config.ts
@@ -36,6 +36,11 @@ export default defineConfig({
             return 'chakra-ui';
           }
 
+          // Chakra UI Icons
+          if (id.includes('@chakra-ui/icons')) {
+            return 'chakra-ui-icons';
+          }
+
           // Split every page into a separate chunk splitting by the last `/`
           if (id.includes('src/pages')) {
             return 'page-' + id.slice(id.lastIndexOf('/') + 1).toLowerCase();
