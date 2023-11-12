f698208e68125a99e4c0129d90f726ddb1d21fc6
Ratchanon Panmas <qq.panmas@gmail.com>
2023-11-12 21:48:53 +0700

    Add Direction component with Google Maps directions service

---
+++ src/components/Direction/Direction.tsx
@@ -0,0 +1,58 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useDirectionsService } from "@vis.gl/react-google-maps";
+import { useEffect } from "react";
+
+interface DirectionProps {
+    origin: google.maps.LatLngLiteral;
+    destination: google.maps.LatLngLiteral;
+}
+
+const Direction = (props: DirectionProps) => {
+
+    const { origin, destination } = props
+
+    const { directionsService, directionsRenderer, } = useDirectionsService({
+        renderOnMap: true,
+    });
+
+
+    useEffect(() => {
+        const getDirection = async () => {
+
+            if (!directionsService || !directionsRenderer) return
+
+            const request: google.maps.DirectionsRequest = {
+                origin: origin,
+                destination: destination,
+                travelMode: google.maps.TravelMode.DRIVING,
+                unitSystem: google.maps.UnitSystem.METRIC,
+                optimizeWaypoints: true,
+                provideRouteAlternatives: false,
+            }
+
+            try {
+                await directionsService.route(request, (response, status) => {
+                    directionsRenderer.setDirections(response)
+                    directionsRenderer.setOptions({
+                        polylineOptions: {
+                            strokeColor: "#2BC875",
+                            strokeOpacity: 1,
+                            strokeWeight: 6,
+                        },
+                        suppressMarkers: true,
+                    })
+                })
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
+        getDirection()
+    }, [directionsRenderer, directionsService])
+
+    return (
+        <div>Direction</div>
+    )
+}
+
+export default Direction
