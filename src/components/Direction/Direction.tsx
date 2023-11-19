51cd397a4fea10b3f380c718112073fba843ac1e
Ratchanon Panmas <qq.panmas@gmail.com>
2023-11-19 23:55:00 +0700

    Refactor Direction component to use useContext hook for directionsRendererOptions

--- src/components/Direction/Direction.tsx
+++ src/components/Direction/Direction.tsx
@@ -1,66 +1,30 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import { DirectionRendererContext } from "@/contexts/RouteContext";
 import { useDirectionsService } from "@vis.gl/react-google-maps";
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
 
-interface DirectionProps {
-    origin: google.maps.LatLngLiteral;
-    destination: google.maps.LatLngLiteral;
-}
+const Direction = () => {
 
-const Direction = (props: DirectionProps) => {
+    const directionsRendererOptions = useContext(DirectionRendererContext);
 
-    const { origin, destination } = props
+    console.log(directionsRendererOptions);
+
 
     const { directionsService, directionsRenderer, } = useDirectionsService({
         renderOnMap: true,
+        renderOptions: {
+            ...directionsRendererOptions,
+            suppressMarkers: true,
+        }
     });
 
 
     useEffect(() => {
-        const getDirection = async () => {
-
-            if (!directionsService || !directionsRenderer) return
-
-            const request: google.maps.DirectionsRequest = {
-                origin: origin,
-                destination: destination,
-                travelMode: google.maps.TravelMode.TRANSIT,
-                unitSystem: google.maps.UnitSystem.METRIC,
-                optimizeWaypoints: true,
-                provideRouteAlternatives: false,
-                language: "th",
-                transitOptions: {
-                    modes: [google.maps.TransitMode.BUS],
-                    routingPreference: google.maps.TransitRoutePreference.LESS_WALKING,
-                    
-                }
-            }
-
-            try {
-                await directionsService.route(request, (response, _) => {
-                    directionsRenderer.setDirections(response)
-
-                    console.log(response)
-
-                    directionsRenderer.setOptions({
-                        polylineOptions: {
-                            strokeColor: "#2BC875",
-                            strokeOpacity: 1,
-                            strokeWeight: 6,
-                        },
-                        suppressMarkers: true,
-                    })
-
-                    directionsRenderer.setPanel(document.getElementById("directions-panel") as HTMLElement)
-
-                })
-            } catch (error) {
-                console.log(error)
-            }
-        }
-
-        getDirection()
-    }, [destination, directionsRenderer, directionsService, origin])
+        if (!directionsService) return;
+        if (!directionsRendererOptions) return;
+        if (!directionsRendererOptions.directions) return;
+        directionsRenderer?.setDirections(directionsRendererOptions.directions);
+    }, [directionsRendererOptions, directionsRenderer, directionsService]);
 
     return (
         <></>
