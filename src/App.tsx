d09248b2a573d658fc416b19c435baf1fb632a26
Ratchanon Panmas <qq.panmas@gmail.com>
2023-11-19 23:54:55 +0700

    Add DirectionRenderer context and provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { APIProvider } from '@vis.gl/react-google-maps'
 import { onAuthStateChanged } from 'firebase/auth'
 import { useEffect, useReducer } from 'react'
 import { RouterProvider } from 'react-router-dom'
+import { DirectionRendererDispatchProvider, DirectionRendererProvider, directionRendererReducer } from './contexts/RouteContext'
 import { UserDispatchProvider, UserProvider, initialUserState, userReducer } from './contexts/UserContext'
 import { auth } from './firebase/firebase'
 import router from './routes/router'
@@ -10,20 +11,25 @@ import router from './routes/router'
 
 function App() {
 
-  const [userData, dispatch] = useReducer(userReducer, initialUserState);
+  const [userData, userDispatch] = useReducer(userReducer, initialUserState);
+  const [directionRenderer, directionRendererDispatch] = useReducer(directionRendererReducer, {} as never);
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
-      dispatch({ type: 'SET_USER', payload: user })
+      userDispatch({ type: 'SET_USER', payload: user })
     })
   }, [])
 
   return (
     <UserProvider value={userData}>
-      <UserDispatchProvider value={dispatch}>
-        <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
-          <RouterProvider router={router} />
-        </APIProvider>
+      <UserDispatchProvider value={userDispatch}>
+        <DirectionRendererProvider value={directionRenderer}>
+          <DirectionRendererDispatchProvider value={directionRendererDispatch}>
+            <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
+              <RouterProvider router={router} />
+            </APIProvider>
+          </DirectionRendererDispatchProvider>
+        </DirectionRendererProvider>
       </UserDispatchProvider>
     </UserProvider>
   )
---
+++ src/contexts/RouteContext.ts
@@ -0,0 +1,35 @@
+import { createContext } from "react";
+
+const intialDirectionRendererOptionContext: google.maps.DirectionsRendererOptions = {};
+
+
+const DirectionRendererContext = createContext<google.maps.DirectionsRendererOptions>(intialDirectionRendererOptionContext);
+const DirectionRendererProvider = DirectionRendererContext.Provider;
+const DirectionRendererConsumer = DirectionRendererContext.Consumer;
+
+type ActionType = 'SET_DIRECTION' | 'CLEAR_DIRECTION' | 'PICK_DIRECTION_INDEX'
+
+interface DirectionRendererAction {
+    type: ActionType
+    payload: google.maps.DirectionsRendererOptions
+}
+
+function directionRendererReducer(rendererOption: google.maps.DirectionsRendererOptions, action: DirectionRendererAction) {
+    switch (action.type) {
+        case 'SET_DIRECTION':
+            return action.payload;
+        case 'CLEAR_DIRECTION':
+            return undefined;
+        case 'PICK_DIRECTION_INDEX':
+            return { ...rendererOption, routeIndex: action.payload.routeIndex };
+        default:
+            throw new Error();
+    }
+}
+
+const DirectionRendererDispatchContext = createContext((() => { }) as React.Dispatch<DirectionRendererAction>);
+const DirectionRendererDispatchProvider = DirectionRendererDispatchContext.Provider;
+const DirectionRendererDispatchConsumer = DirectionRendererDispatchContext.Consumer;
+
+export { DirectionRendererConsumer, DirectionRendererContext, DirectionRendererDispatchConsumer, DirectionRendererDispatchContext, DirectionRendererDispatchProvider, DirectionRendererProvider, directionRendererReducer, intialDirectionRendererOptionContext };
+
