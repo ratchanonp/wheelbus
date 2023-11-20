e9ceb38b0a6504751d65d244e47418d39784d1c9
Ratchanon Panmas <qq.panmas@gmail.com>
2023-11-20 19:59:00 +0700

    Add start and stop markers to the direction component

--- src/components/Direction/Direction.tsx
+++ src/components/Direction/Direction.tsx
@@ -1,7 +1,9 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { DirectionRendererContext } from "@/contexts/RouteContext";
-import { useDirectionsService } from "@vis.gl/react-google-maps";
+import { AdvancedMarker, useDirectionsService } from "@vis.gl/react-google-maps";
 import { useContext, useEffect } from "react";
+import StartMarker from "../CustomMarker/StartMarker";
+import StopMarker from "../CustomMarker/StopMarker";
 
 const Direction = () => {
 
@@ -15,7 +17,7 @@ const Direction = () => {
         renderOptions: {
             ...directionsRendererOptions,
             suppressMarkers: true,
-        }
+        },
     });
 
 
@@ -26,8 +28,14 @@ const Direction = () => {
         directionsRenderer?.setDirections(directionsRendererOptions.directions);
     }, [directionsRendererOptions]);
 
+    const startLocation = directionsRendererOptions.directions?.routes[0].legs[0].start_location;
+    const endLocation = directionsRendererOptions.directions?.routes[0].legs[0].end_location;
+
     return (
-        <></>
+        <>
+            <AdvancedMarker position={startLocation} > <StartMarker /> </AdvancedMarker>
+            <AdvancedMarker position={endLocation} > <StopMarker /> </AdvancedMarker>
+        </>
     )
 }
 
