e473ef69e16c4952561d50a72b202ec19c05b110
Ratchanon Panmas <qq.panmas@gmail.com>
2023-11-20 14:41:08 +0700

    Add context providers for search and place suggestions

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import { APIProvider } from '@vis.gl/react-google-maps'
 import { onAuthStateChanged } from 'firebase/auth'
 import { useEffect, useReducer } from 'react'
 import { RouterProvider } from 'react-router-dom'
+import { PlaceSuggestionContextProvider, PlaceSuggestionReducerProvider, initialPlaceSuggestionContextData, placeSuggestionReducer } from './contexts/PlaceSuggestionContext'
 import { DirectionRendererDispatchProvider, DirectionRendererProvider, directionRendererReducer } from './contexts/RouteContext'
+import { SearchContextProvider, SearchDispatchProvider, initialSearchContextData, searchReducer } from './contexts/SearchContext'
 import { UserDispatchProvider, UserProvider, initialUserState, userReducer } from './contexts/UserContext'
 import { auth } from './firebase/firebase'
 import router from './routes/router'
@@ -13,6 +15,8 @@ function App() {
 
   const [userData, userDispatch] = useReducer(userReducer, initialUserState);
   const [directionRenderer, directionRendererDispatch] = useReducer(directionRendererReducer, {} as never);
+  const [searchData, searchDispatch] = useReducer(searchReducer, initialSearchContextData);
+  const [placeSuggestions, placeSuggestionsDispatch] = useReducer(placeSuggestionReducer, initialPlaceSuggestionContextData);
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
@@ -25,9 +29,17 @@ function App() {
       <UserDispatchProvider value={userDispatch}>
         <DirectionRendererProvider value={directionRenderer}>
           <DirectionRendererDispatchProvider value={directionRendererDispatch}>
-            <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
-              <RouterProvider router={router} />
-            </APIProvider>
+            <SearchContextProvider value={searchData}>
+              <SearchDispatchProvider value={searchDispatch}>
+                <PlaceSuggestionContextProvider value={placeSuggestions}>
+                  <PlaceSuggestionReducerProvider value={placeSuggestionsDispatch}>
+                    <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
+                      <RouterProvider router={router} />
+                    </APIProvider>
+                  </PlaceSuggestionReducerProvider>
+                </PlaceSuggestionContextProvider>
+              </SearchDispatchProvider>
+            </SearchContextProvider>
           </DirectionRendererDispatchProvider>
         </DirectionRendererProvider>
       </UserDispatchProvider>
