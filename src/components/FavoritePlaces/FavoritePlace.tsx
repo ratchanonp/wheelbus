cf9bf00a08ae84876f7eb3ac3f0925b2b43ae86f
Ratchanon Panmas <qq.panmas@gmail.com>
2023-11-20 19:00:19 +0700

    Add search functionality to FavoritePlace component

--- src/components/FavoritePlaces/FavoritePlace.tsx
+++ src/components/FavoritePlaces/FavoritePlace.tsx
@@ -1,12 +1,33 @@
+import { SearchContext, SearchDispatchContext } from "@/contexts/SearchContext";
 import useGetFavoritePlaces from "@/hooks/useGetFavoritePlaces";
 import { favoritePlacesGroupByCategory } from "@/utils/favorite";
 import { Divider, Flex, Heading, Stack } from "@chakra-ui/react";
+import { useContext } from "react";
 import FavoritePlaceItemLoading from "./FavoritePlaceItem.loading";
 
 const FavoritePlace = () => {
 
     const { favoritePlaces, isLoading, error } = useGetFavoritePlaces();
     const favoritePlaceByCategory = favoritePlacesGroupByCategory(favoritePlaces);
+
+    const search = useContext(SearchContext);
+    const searchDispatch = useContext(SearchDispatchContext);
+
+    const { focusedInput } = search;
+
+    const handleSelectFavoritePlace = (name: string, placeId: string) => {
+
+        const customName = "สถานที่โปรด - " + name;
+
+        if (focusedInput === "from") {
+            searchDispatch({ type: "SET_FROM", payload: { from: customName } })
+            searchDispatch({ type: "SET_FROM_PLACE_ID", payload: { fromPlaceId: placeId } })
+        }
+        if (focusedInput === "to") {
+            searchDispatch({ type: "SET_TO", payload: { to: customName } })
+            searchDispatch({ type: "SET_TO_PLACE_ID", payload: { toPlaceId: placeId } })
+        }
+    }
 
     if (error) return <div>Error</div>
 
@@ -16,10 +37,10 @@ const FavoritePlace = () => {
                 <Heading size="md" fontWeight="semibold">🏠 บ้าน</Heading>
                 {isLoading && (<FavoritePlaceItemLoading noItems={2} />)}
                 {favoritePlaceByCategory.HOME && favoritePlaceByCategory.HOME.map((favoritePlace, idx) => {
-                    const { name } = favoritePlace;
+                    const { name, placeId } = favoritePlace;
 
                     return (
-                        <Flex key={idx} justifyContent="space-between" alignItems="center">
+                        <Flex key={idx} justifyContent="space-between" alignItems="center" onClick={() => handleSelectFavoritePlace(name, placeId)}>
                             <Heading size="sm" fontWeight="medium">{name}</Heading>
                         </Flex>
                     )
@@ -29,10 +50,10 @@ const FavoritePlace = () => {
                 <Heading size="md" fontWeight="semibold">💼 ที่ทำงาน</Heading>
                 {isLoading && (<FavoritePlaceItemLoading noItems={1} />)}
                 {favoritePlaceByCategory.WORK && favoritePlaceByCategory.WORK.map((favoritePlace, idx) => {
-                    const { name } = favoritePlace;
+                    const { name, placeId } = favoritePlace;
 
                     return (
-                        <Flex key={idx} justifyContent="space-between" alignItems="center">
+                        <Flex key={idx} justifyContent="space-between" alignItems="center" onClick={() => handleSelectFavoritePlace(name, placeId)}>
                             <Heading size="sm" fontWeight="medium">{name}</Heading>
                         </Flex>
                     )
@@ -42,16 +63,16 @@ const FavoritePlace = () => {
                 <Heading size="md" fontWeight="semibold">⭐️ ที่โปรด</Heading>
                 {isLoading && (<FavoritePlaceItemLoading noItems={1} />)}
                 {favoritePlaceByCategory.OTHER && favoritePlaceByCategory.OTHER.map((favoritePlace, idx) => {
-                    const { name } = favoritePlace;
+                    const { name, placeId } = favoritePlace;
 
                     return (
-                        <Flex key={idx} justifyContent="space-between" alignItems="center">
+                        <Flex key={idx} justifyContent="space-between" alignItems="center" onClick={() => handleSelectFavoritePlace(name, placeId)}>
                             <Heading size="sm" fontWeight="medium">{name}</Heading>
                         </Flex>
                     )
                 })}
             </Stack>
-        </Stack>
+        </Stack >
     )
 }
 
