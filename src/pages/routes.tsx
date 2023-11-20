7b7288390b553946c071b11dda3e5374e0cf60bd
Ratchanon Panmas <qq.panmas@gmail.com>
2023-11-20 14:42:14 +0700

    Refactor Route Search

--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -1,138 +1,34 @@
-import StepDetail from "@/components/Step/StepDetail";
-import { DirectionRendererContext, DirectionRendererDispatchContext } from "@/contexts/RouteContext";
-import useGetDirection from "@/hooks/useGetDirection";
-import { Box, Circle, Divider, Flex, Heading, Icon, IconButton, Input, InputGroup, InputLeftElement, Stack, Text } from "@chakra-ui/react";
-import { useDebounce } from "@uidotdev/usehooks";
-import { useMapsLibrary } from "@vis.gl/react-google-maps";
-import { useContext, useEffect, useState } from "react";
-import { FaArrowLeft } from "react-icons/fa";
-import { FaChevronRight, FaLocationDot } from "react-icons/fa6";
+import DirectionResult from "@/components/Direction/DirectionResult/DirectionResult";
+import FavoritePlace from "@/components/FavoritePlaces/FavoritePlace";
+import PlaceSuggestion from "@/components/PlaceSuggestion/PlaceSuggestion";
+import { CLEAR_PLACE_SUGGESTIONS, PlaceSuggestionContext, PlaceSuggestionReducerContext } from "@/contexts/PlaceSuggestionContext";
+import { CLEAR_FROM, CLEAR_TO, SET_FOCUSED_INPUT, SET_FROM, SET_TO, SWAP, SearchContext, SearchDispatchContext, focusedInputType } from "@/contexts/SearchContext";
+import { Box, Circle, Flex, Heading, Icon, IconButton, Input, InputGroup, InputLeftElement, InputRightElement, Stack, Text } from "@chakra-ui/react";
+import { useContext } from "react";
+import { FaArrowLeft, FaTimesCircle } from "react-icons/fa";
+import { FaLocationDot } from "react-icons/fa6";
 import { MdSwapVert } from "react-icons/md";
-import { Link as RouterLink, useNavigate } from "react-router-dom";
+import { Link as RouterLink } from "react-router-dom";
 
 const RouteSearchPage = () => {
 
-    // Input State
-    const [from, setFrom] = useState<string>("");
-    const [to, setTo] = useState<string>("");
+    const search = useContext(SearchContext);
+    const searchDispatch = useContext(SearchDispatchContext);
 
-    const fromDebounce = useDebounce(from, 300);
-    const toDebounce = useDebounce(to, 300);
+    const placeSuggestions = useContext(PlaceSuggestionContext);
+    const placeSuggestionsDispatch = useContext(PlaceSuggestionReducerContext);
 
-    const [fromPlaceId, setFromPlaceId] = useState<string | undefined>(undefined);
-    const [toPlaceId, setToPlaceId] = useState<string | undefined>(undefined);
+    const { from, to, focusedInput } = search;
 
-    const [fieldFocus, setFieldFocus] = useState<"from" | "to" | undefined>(undefined);
-
-    const placeLibrary = useMapsLibrary("places");
-    const [placeSuggestions, setPlaceSuggestions] = useState<google.maps.places.AutocompletePrediction[]>([]);
-
-    const { direction, getDirection } = useGetDirection();
-
-    const dispatch = useContext(DirectionRendererDispatchContext);
-    const directionRenderer = useContext(DirectionRendererContext);
-
-    const navigate = useNavigate();
-
-
-    useEffect(() => {
-        const placeSuggesttions = async () => {
-            if (!placeLibrary) return null;
-            if (fieldFocus === undefined) return null;
-
-            if (fromDebounce || toDebounce) {
-                const prediction = new placeLibrary.AutocompleteService();
-
-                const predictionRequest: google.maps.places.AutocompletionRequest = {
-                    input: fieldFocus === "from" ? fromDebounce : toDebounce,
-                    language: "th",
-                    componentRestrictions: {
-                        country: "th"
-                    }
-                }
-
-                prediction.getPlacePredictions(predictionRequest, (results, status) => {
-                    if (status === google.maps.places.PlacesServiceStatus.OK) {
-                        setPlaceSuggestions(results || []);
-                    }
-                })
-            }
+    const handleClear = (focusedInput: focusedInputType) => {
+        if (focusedInput === "from") {
+            searchDispatch({ type: CLEAR_FROM })
+        }
+        if (focusedInput === "to") {
+            searchDispatch({ type: CLEAR_TO })
         }
 
-        placeSuggesttions();
-    }, [fromDebounce, toDebounce])
-
-    useEffect(() => {
-        if (fromPlaceId && toPlaceId) {
-            getDirection(fromPlaceId, toPlaceId);
-        }
-    }, [fromPlaceId, toPlaceId])
-
-    useEffect(() => {
-        if (direction) {
-            const option: google.maps.DirectionsRendererOptions = {
-                directions: direction,
-            }
-
-            console.log("set option", option);
-
-            dispatch({
-                type: "SET_DIRECTION",
-                payload: option
-            })
-
-            console.log("directionRenderer", directionRenderer);
-        }
-    }, [direction])
-
-
-    const handleSelectSuggestion = async (placeId: string) => {
-        if (!placeLibrary) return null;
-
-        console.log(placeId);
-
-        const placeDetail = new placeLibrary.PlacesService(document.createElement("div"));
-
-        // Get Place Name
-        const placeDetailRequest: google.maps.places.PlaceDetailsRequest = {
-            placeId: placeId,
-            fields: ["name"],
-            language: "th"
-        }
-
-        placeDetail.getDetails(placeDetailRequest, (place, status) => {
-            if (status === google.maps.places.PlacesServiceStatus.OK) {
-                if (fieldFocus === "from") {
-                    setFrom(place?.name || "");
-                    setFromPlaceId(placeId);
-                } else if (fieldFocus === "to") {
-                    setTo(place?.name || "");
-                    setToPlaceId(placeId);
-                }
-                setFieldFocus(undefined);
-            }
-        })
-
-
-
-
-
-        setPlaceSuggestions([]);
-    }
-
-    const handleSelectionRouteOption = (routeIndex: number) => {
-        if (!direction) return null;
-
-        const option: google.maps.DirectionsRendererOptions = {
-            routeIndex: routeIndex,
-        }
-
-        dispatch({
-            type: "PICK_DIRECTION_INDEX",
-            payload: option
-        })
-
-        navigate("/navigation");
+        placeSuggestionsDispatch({ type: CLEAR_PLACE_SUGGESTIONS })
     }
 
     return (
@@ -164,9 +60,17 @@ const RouteSearchPage = () => {
                                 <Input
                                     bgColor="white" type="text" borderLeft="none" placeholder="เริ่มจากที่ไหน" fontFamily="prompt" color="gray" _focusVisible={{ border: "none" }}
                                     value={from}
-                                    onChange={(e) => { e.target.value !== from && setFrom(e.target.value) }}
-                                    onFocus={() => setFieldFocus("from")}
+                                    onChange={(e) => { searchDispatch({ type: SET_FROM, payload: { from: e.target.value } }) }}
+                                    onFocus={() => searchDispatch({ type: SET_FOCUSED_INPUT, payload: { focusedInput: "from" } })}
                                 />
+                                {from && (
+                                    <InputRightElement>
+                                        <Icon
+                                            as={FaTimesCircle} color="gray" w={4} h={4}
+                                            onClick={() => handleClear("from")}
+                                        />
+                                    </InputRightElement>
+                                )}
                             </InputGroup>
                         </Flex>
                         <Flex align="center" gap={3}>
@@ -176,9 +80,17 @@ const RouteSearchPage = () => {
                                 <Input
                                     bgColor="white" type="text" borderLeft="none" placeholder="ไปที่ไหนดี" fontFamily="prompt" color="gray" _focusVisible={{ border: "none" }}
                                     value={to}
-                                    onChange={(e) => { e.target.value !== to && setTo(e.target.value) }}
-                                    onFocus={() => setFieldFocus("to")}
+                                    onChange={(e) => { searchDispatch({ type: SET_TO, payload: { to: e.target.value } }) }}
+                                    onFocus={() => searchDispatch({ type: SET_FOCUSED_INPUT, payload: { focusedInput: "to" } })}
                                 />
+                                {to && (
+                                    <InputRightElement>
+                                        <Icon
+                                            as={FaTimesCircle} color="gray" w={4} h={4}
+                                            onClick={() => handleClear("to")}
+                                        />
+                                    </InputRightElement>
+                                )}
                             </InputGroup>
                         </Flex>
                     </Stack>
@@ -191,63 +103,24 @@ const RouteSearchPage = () => {
                             />}
                             aria-label="swap"
                             size="md"
-                            variant="ghost"
+                            variant="none"
+                            onClick={() => searchDispatch({ type: SWAP })}
                         />
                     </Flex>
                 </Flex>
             </Stack>
             <Flex flex={1} direction="column">
-                {fieldFocus && (
+                {focusedInput && placeSuggestions.placeSuggestions.length == 0 && (
                     <Stack p={5} spacing={5} fontFamily="prompt">
                         <Flex justify="space-between">
                             <Heading size="md" fontWeight="semibold">เลือกบนแผนที่</Heading>
                             <Icon as={FaLocationDot} color="slate" w={4} h={4} />
                         </Flex>
-                    </Stack>)}
-                <Stack p={3}>
-                    {placeSuggestions && placeSuggestions.length > 0 && (
-                        <Stack divider={<Divider />} >
-                            {
-                                placeSuggestions && placeSuggestions.map((place, index) => (
-                                    <Box key={index} p={2} _hover={{ bgColor: "brand.500", color: "white" }} cursor="pointer" onClick={() => handleSelectSuggestion(place.place_id)}>
-                                        <Text fontFamily="prompt" fontSize="sm">{place.description}</Text>
-                                    </Box>
-                                ))
-                            }
-                        </Stack>
-                    )}
-                    {!fieldFocus && direction && direction.routes && direction.routes.length > 0 && (
-                        <Stack p={3} divider={<Divider />} spacing={3}>
-                            {direction?.routes.map((route, index) => {
-                                const { legs } = route;
-                                const data = legs[0];
-
-                                const { duration, steps, departure_time, arrival_time } = data;
-
-                                return (
-                                    <Flex key={index} gap={5} direction="column" onClick={() => handleSelectionRouteOption(index)} cursor="pointer">
-                                        <Flex mr={2} flexWrap={"wrap"} gap={2}>
-                                            {steps.map((step, index) => {
-                                                return (
-                                                    <>
-                                                        <StepDetail key={index} step={step} />
-                                                        {index !== steps.length - 1 && (
-                                                            <Icon as={FaChevronRight} color="slate" w={4} h={4} />
-                                                        )}
-                                                    </>
-                                                )
-                                            })}
-                                        </Flex>
-                                        <Flex justifyContent="space-between">
-                                            <Text>{departure_time?.text} - {arrival_time?.text}</Text>
-                                            <Text>{duration?.text}</Text>
-                                        </Flex>
-                                    </Flex>
-                                )
-                            })}
-                        </Stack>
-                    )}
-                </Stack>
+                        <FavoritePlace />
+                    </Stack>
+                )}
+                <PlaceSuggestion />
+                <DirectionResult />
             </Flex>
 
         </Flex>
