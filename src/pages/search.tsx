a8d1f5fcae4b183d29da0c8bd8b5aadbc39a7fb4
Ratchanon Panmas <qq.panmas@gmail.com>
2023-11-21 09:32:18 +0700

    Update search page UI and add current location button

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -4,19 +4,19 @@ import StartMarker from '@/components/CustomMarker/StartMarker';
 import SideDrawerContent from '@/components/SideDrawer/SideDrawer';
 import useCurrentLocation from '@/hooks/useCurrentLocation';
 import { HamburgerIcon, WarningTwoIcon } from '@chakra-ui/icons';
-import { Box, Container, Flex, Heading, IconButton, Image, Link, Spinner, Stack, Text, useDisclosure } from "@chakra-ui/react";
+import { Box, Container, Divider, Flex, Heading, Icon, IconButton, Image, Link, Stack, Text, useDisclosure } from "@chakra-ui/react";
 import { Map } from "@vis.gl/react-google-maps";
-import { useMemo } from 'react';
+import { useEffect, useMemo } from 'react';
+import { FaLocationCrosshairs } from 'react-icons/fa6';
 import { Link as RouterLink } from 'react-router-dom';
 
 function SerchPage() {
 
-    const { location, loading, error } = useCurrentLocation();
+    const { location, loading, error, getCurrentLocation } = useCurrentLocation();
     const { latitude, longitude } = location.coords;
 
     const { isOpen, onOpen, onClose } = useDisclosure();
 
-    console.log(location)
 
     const currentLocation: google.maps.LatLngLiteral = useMemo(() => {
         return {
@@ -25,26 +25,24 @@ function SerchPage() {
         }
     }, [latitude, longitude])
 
+    useEffect(() => {
+        getCurrentLocation();
+    }, [])
+
+
     return (
         <Container maxW="container.sm" display="flex" h="100svh" w="100svw" p={0} position="relative" overflow="hidden">
             <SideDrawerContent onClose={onClose} isOpen={isOpen} />
             <Flex w="full" h="100svh" zIndex={0} justify="center" align="center">
-                {loading && (
-                    <Flex direction="column" justify="center" align="center" gap={5}>
-                        <Spinner size="xl" color="brand.500" />
-                        <Text fontFamily="prompt" color="gray.500" ml={3}>กำลังค้นหาตำแหน่งปัจจุบันของคุณ</Text>
-                    </Flex>
-                )}
-                {!loading && !error && (
-                    <Map
-                        center={currentLocation}
-                        zoom={16}
-                        disableDefaultUI={true}
-                        mapId="19388afc054dab84"
-                    >
-                        <CustomMarker position={currentLocation} pin={<StartMarker />} />
-                    </Map>
-                )}
+                <Map
+                    center={currentLocation}
+                    zoom={16}
+                    disableDefaultUI={true}
+                    mapId="19388afc054dab84"
+                    id="main-map"
+                >
+                    <CustomMarker position={currentLocation} pin={<StartMarker />} />
+                </Map>
                 {error && (
                     <Flex direction="column" justify="center" align="center" gap={5}>
                         <WarningTwoIcon w={10} h={10} color="red.500" />
@@ -69,6 +67,9 @@ function SerchPage() {
                     </Link>
                 </Stack>
             </Flex>
+            <Stack position="absolute" right={5} bottom="100px" bgColor="white" borderRadius="lg" shadow="xl" divider={<Divider />} spacing={0}>
+                <IconButton variant="none" icon={<Icon as={FaLocationCrosshairs} w={6} h={6} />} aria-label="Get Current Location" color="brand.500" onClick={getCurrentLocation} isLoading={loading} />
+            </Stack>
         </Container >
     )
 }
